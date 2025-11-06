84d20209a86045c91aedbb840b5d2521cac0005d
Robin Malfait <malfait.robin@gmail.com>
2025-11-06 23:07:10 +0100

    format

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::{Parser, Subcommand};
-use color_eyre::eyre::{eyre, Result};
+use color_eyre::eyre::{Result, eyre};
 use colored::*;
 use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
